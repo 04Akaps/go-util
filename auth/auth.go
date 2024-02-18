490024a632c36c8b323c8f3a3daf842388fbb626
yuhojin <sdl155@naver.com>
2024-02-18 15:58:57 +0900

    Feat: add init for protoc

--- auth/auth.go
+++ auth/auth.go
@@ -11,19 +11,24 @@ type Auth struct {
 	*client.AuthGrpcClient
 }
 
-func NewAuth(serverUrl, clientUrl, pasetoKey string) (*Auth, error) {
+func NewAuth(serverUrl, clientUrl, pasetoKey string, init bool) (*Auth, error) {
 	a := new(Auth)
+	var err error
 
-	cmd := exec.Command("protoc",
-		"--go_out=.",
-		"--go_opt=paths=source_relative",
-		"--go-grpc_out=.",
-		"--go-grpc_opt=paths=source_relative",
-		"proto/auth.proto")
+	if init {
+		cmd := exec.Command("protoc",
+			"--go_out=.",
+			"--go_opt=paths=source_relative",
+			"--go-grpc_out=.",
+			"--go-grpc_opt=paths=source_relative",
+			"proto/auth.proto")
 
-	if _, err := cmd.CombinedOutput(); err != nil {
-		panic(err)
-	} else if a.AuthGrpcClient, err = client.NewGrpcClient(clientUrl, pasetoKey); err != nil {
+		if _, err := cmd.CombinedOutput(); err != nil {
+			panic(err)
+		}
+	}
+
+	if a.AuthGrpcClient, err = client.NewGrpcClient(clientUrl, pasetoKey); err != nil {
 		panic(err)
 	} else {
 		server.NewGrpcServer(serverUrl, pasetoKey)
